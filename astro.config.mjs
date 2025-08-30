d5a2424fe7f37901f1d22c1ac9d494c7e468ac22
N I_temp <crts5fld@gmail.com>
2025-08-30 16:52:28 +0900

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import decapCmsOauth from 'astro-decap-cms-oauth';
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pr0p0se.com',
 	output: 'static',
-	integrations: [mdx(), sitemap(), decapCmsOauth()],
-	adapter: vercel(),
+	integrations: [mdx(), sitemap()],
 });
