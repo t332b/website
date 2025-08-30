bc4237222c0bdc85c377abd0f59121a95e2b0ddd
N I_temp <crts5fld@gmail.com>
2025-08-30 15:51:15 +0900

    サイトURLを「https://pr0p0se.com」に変更し、FTPデプロイ用のGitHub Actionsワークフローを追加。日記ページの新規作成とブログ一覧ページを実装し、著者情報の取得機能を追加。

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pr0p0se.com',
 	output: 'static',
 	integrations: [mdx(), sitemap(), decapCmsOauth()],
 	adapter: vercel(),
